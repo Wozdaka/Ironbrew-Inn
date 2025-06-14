5fabe91990d2f7804d640aad1a0113b8cf85ac91
Wozdaka <47612706+Wozdaka@users.noreply.github.com>
2025-06-14 11:07:12 -0700

    Update tools.h
    added World/Zone/tile updates
    added Fog Of War Initial update
    Bug Fixes
    General tweaks

--- tools.h
+++ tools.h
@@ -6,11 +6,22 @@
 #define TOOLS_H
 
 
+#define LAST_MSG_SIZE 255
+
+extern char last_action_msg[LAST_MSG_SIZE];
+
 // ============================
 // Function declarations
 // ============================
 
 // helper function replaces putchar so it allows us to colorize a single char - fancy
 void print_color_char(char c);
+
+
+
+
+// for adding the last action string added to the buffer for displaying  later.
+void set_last_action_msg(const char *format, ...);
+
 
 #endif
